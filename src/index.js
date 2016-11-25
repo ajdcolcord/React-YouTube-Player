02af24429528c78e7f0624adc272e001d26d8444
Austin Colcord <ajdcolcord@gmail.com>
2016-11-25 17:23:37 -0500

    install npm, remove starter src files

--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+/**
+ * Created by Austin on 11/25/16.
+ */
+/** Creating a new component, that should produce some sort of HTML */
 
-import App from './components/app';
-import reducers from './reducers';
-
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Take this component's generated HTML and put it on the page (in the DOM)
