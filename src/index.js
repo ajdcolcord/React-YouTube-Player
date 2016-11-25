96914de803f6407f2a8d2076ee99888a7fa16d9f
Austin Colcord <ajdcolcord@gmail.com>
2016-11-25 17:26:21 -0500

    Ignore .idea files

--- src/index.js
+++ src/index.js
@@ -3,4 +3,8 @@
  */
 /** Creating a new component, that should produce some sort of HTML */
 
+const App = function() {
+    return <div>Hi!</div>;
+}
+
 // Take this component's generated HTML and put it on the page (in the DOM)
