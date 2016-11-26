5abbabd4cedf3fadfd6dd5244abdbb96a58aa820
Austin Colcord <ajdcolcord@gmail.com>
2016-11-25 23:01:36 -0500

    simplify event handler function

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -5,8 +5,9 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
     render() {
-        return <input />;
+        return <input onChange={event => console.log(event.target.value)} />;
     }
+
 }
 
 
