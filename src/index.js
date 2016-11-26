39ecfc76ab843409d4279594be0543dd448b11ec
Austin Colcord <ajdcolcord@gmail.com>
2016-11-25 23:47:24 -0500

    continuing app youtube search modeling

--- src/index.js
+++ src/index.js
@@ -1,27 +1,32 @@
 /**
  * Created by Austin on 11/25/16.
  */
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-
 import SearchBar from './components/search_bar';
-
 const API_KEY = "AIzaSyCf0FaXPs-HqJm93mgTW1g4vxE8JurUmUU";
 
-YTSearch({key: API_KEY, term: "surfboards"}, function(data) {
-    console.log(data);
-});
 
-/** Creating a new component, that should produce some sort of HTML */
 
-const App = () => {
-    return (
-    <div>
-        <SearchBar />
-    </div>
-    );
-};
+class App extends Component {
+    constructor(props) {
+        super(props);
 
-// Take this component's generated HTML and put it on the page (in the DOM)
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term: "surfboards"}, videos => {
+            this.setState({ videos })
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar/>
+            </div>
+        );
+    }
+}
+
 ReactDOM.render(<App />, document.querySelector('.container'));
